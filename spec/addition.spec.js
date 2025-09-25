984e3ec04fc2ed2e7f290b05e36e3ae5bb56d7b0
tanishqbhadouria <145770484+tanishqbhadouria@users.noreply.github.com>
2025-09-25 13:07:29 +0530

    Add files via upload

---
+++ spec/addition.spec.js
@@ -0,0 +1,5 @@
+describe('Simple math sanity check', function() {
+  it('adds 2 + 2 = 4', function() {
+    expect(2 + 2).toBe(4);
+  });
+});
---
+++ spec/support/jasmine-browser.mjs
@@ -0,0 +1,12 @@
+export default {
+  specDir: "spec",
+  srcDir: "src",
+  srcFiles: [
+    "extjs-setup.js",
+  ],
+  specFiles: [
+    "**/*[sS]pec.?(m)js"
+  ],
+  random: false,
+  browser: "firefox" 
+};
